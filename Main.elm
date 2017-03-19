a4fce5383cf71e96c358b5889dc2c48174abf966
tony-jones <ad30jone@gmail.com>
2017-03-19 00:31:09 -0400

    Refactor styling into individual functions

--- Main.elm
+++ Main.elm
@@ -3,67 +3,44 @@ module Main exposing (..)
 import Html
 import Html.Attributes
 
+{- other page elements -}
 main = Html.div
-  [ Html.Attributes.style [("text-align", "center")] ]
+  [ pageStyling ]
   [
-    Html.h1
-      [ Html.Attributes.style [("font-size", "2em"), ("color", "#333"), ("padding-bottom", "30px")] ]
-      [Html.text "cats"],
-    Html.button
-      [
-        Html.Attributes.style
-          [
-            ("padding", "10px"),
-            ("background-color", "#99ddff"),
-            ("border-radius", "2px"),
-            ("border", "1px solid #99ddff"),
-            ("color", "white"),
-            ("font-size", "1.5em"),
-            ("cursor", "pointer"),
-            ("margin", "5px")
-          ]
-      ]
-      [Html.text "cats"],
-    Html.button
-      [
-        Html.Attributes.style
-          [
-            ("padding", "10px"),
-            ("background-color", "#99ddff"),
-            ("border-radius", "2px"),
-            ("border", "1px solid #99ddff"),
-            ("color", "white"),
-            ("font-size", "1.5em"),
-            ("cursor", "pointer"),
-            ("margin", "5px")
-          ]
-      ]
-      [Html.text "dogs"],
-    Html.button
-      [
-        Html.Attributes.style
-          [
-            ("padding", "10px"),
-            ("background-color", "#99ddff"),
-            ("border-radius", "2px"),
-            ("border", "1px solid #99ddff"),
-            ("color", "white"),
-            ("font-size", "1.5em"),
-            ("cursor", "pointer"),
-            ("margin", "5px")
-          ]
-      ]
-      [Html.text "ice cream"],
+    Html.h1 [ headingStyling ] [Html.text "cats"],
+    Html.button [ buttonStyling ] [Html.text "cats"],
+    Html.button [ buttonStyling ] [Html.text "dogs"],
+    Html.button [ buttonStyling ] [Html.text "ice cream"],
     Html.br [] [],
     Html.br [] [],
-    Html.img
-      [
-        Html.Attributes.style
-          [
-            ("border-radius", "2px"),
-            ("box-shadow", "0 2px 4px rgba(0,0,0,0.12), 0 2px 3px rgba(0,0,0,0.24)")
-          ],
-        Html.Attributes.src "https://media.giphy.com/media/11s7Ke7jcNxCHS/giphy.gif"
-      ]
-      []
+    Html.img [ imageStyling, Html.Attributes.src "https://media.giphy.com/media/11s7Ke7jcNxCHS/giphy.gif" ] []
   ]
+
+pageStyling = Html.Attributes.style [ ("text-align", "center") ]
+headingStyling =
+  Html.Attributes.style
+    [
+      ("font-size", "2em"),
+      ("color", "#333"),
+      ("padding-bottom", "30px")
+    ]
+
+buttonStyling =
+  Html.Attributes.style
+    [
+      ("padding", "10px"),
+      ("background-color", "#99ddff"),
+      ("border-radius", "2px"),
+      ("border", "1px solid #99ddff"),
+      ("color", "white"),
+      ("font-size", "1.5em"),
+      ("margin", "5px"),
+      ("cursor", "pointer")
+    ]
+
+imageStyling =
+  Html.Attributes.style
+    [
+      ("border-radius", "2px"),
+      ("box-shadow", "0 2px 4px rgba(0,0,0,0.12), 0 2px 3px rgba(0,0,0,0.24)")
+    ]
