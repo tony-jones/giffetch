1fd519017faa8b42698dbcbafceb53c99f55c9fa
tony-jones <ad30jone@gmail.com>
2017-03-21 21:50:05 -0400

    adding gliphy api stuff

--- Main.elm
+++ Main.elm
@@ -1,74 +1,144 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
 
-main = Html.beginnerProgram { model = model,
-                              view = view,
-                              update = update }
 
-type Message = AskedForCats | AskedForDogs | AskedForIceCream
+main =
+    Html.program
+        { init = init "cats"
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
-catPicture = "https://media.giphy.com/media/ND6xkVPaj8tHO/giphy.gif"
-dogPicture = "https://media.giphy.com/media/oJWx7MtpR2qdi/giphy.gif"
-iceCreamPicture = "https://media.giphy.com/media/QjagU0ONoQwCc/giphy.gif" 
 
-type alias Model = { title : String, picture : String }
-model : Model
-model = Model "cats" "https://media.giphy.com/media/ND6xkVPaj8tHO/giphy.gif"
+type alias Model =
+    { topic : String
+    , gifUrl : String
+    , searchText : String
+    }
+
+
+init : String -> ( Model, Cmd Message )
+init topic = ( Model "" "" "", getRandomGif topic )
+
+
+type Message
+    = AskedForNewPic
+    | SearchTextChanged String
+    | NewGifReceived (Result Http.Error String)
+
+
+update : Message -> Model -> ( Model, Cmd Message )
+update message model =
+    case message of
+        SearchTextChanged newText ->
+            ( { model | searchText = newText }, Cmd.none )
+
+        AskedForNewPic ->
+            if String.length model.searchText < 1 then
+                ( model, getRandomGif model.topic )
+            else
+                ( { model
+                    | searchText = ""
+                    , topic = model.searchText
+                  }
+                , getRandomGif model.searchText
+                )
+
+        NewGifReceived (Ok newImageUrl) ->
+            ( Model model.topic newImageUrl "", Cmd.none )
+
+        NewGifReceived (Err _ ) ->
+            ( model, Cmd.none )
+
 
 view : Model -> Html Message
-view model = div
-  [ pageStyling ]
-  [
-    h1 [ headingStyling ] [ text model.title ],
-    button [ buttonStyling, onClick AskedForCats ] [ text "cats" ],
-    button [ buttonStyling, onClick AskedForDogs ] [ text "dogs" ],
-    button [ buttonStyling, onClick AskedForIceCream ] [ text "ice cream" ],
-    br [] [],
-    br [] [],
-    img [ imageStyling, src model.picture ] []
-  ]
+view model =
+    div [ pageStyle ]
+        [ h2 [ titleStyle ]
+            [ text model.topic ]
+        , input
+            [ inputStyle
+            , value model.searchText
+            , onInput SearchTextChanged
+            , placeholder "search"
+            ]
+            []
+        , br [] []
+        , br [] []
+        , button [ buttonStyle, onClick AskedForNewPic ]
+            [ text "more please!" ]
+        , br [] []
+        , br [] []
+        , img [ imgStyle, src model.gifUrl ] []
+        ]
 
-update : Message -> Model -> Model
-update message model =
-  case message of
-    AskedForCats     -> { model | title = "cats",
-                                  picture = catPicture }
 
-    AskedForDogs     -> { model | title = "dogs",
-                                  picture = dogPicture }
+subscriptions : Model -> Sub Message
+subscriptions model =
+    Sub.none
 
-    AskedForIceCream -> { model | title = "ice cream",
-                                  picture = iceCreamPicture }
--- Styling
 
-pageStyling =
-    style [ ( "text-align", "center" ) ]
+getRandomGif : String -> Cmd Message
+getRandomGif topic =
+    let
+        url =
+            "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="
+                ++ topic
+    in
+        Http.send NewGifReceived (Http.get url decodeGifUrl)
 
-headingStyling =
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+    Decode.at [ "data", "image_url" ] Decode.string
+
+
+
+-- Styles
+
+
+pageStyle =
+    style [ ( "text-align", "center" ), ( "font-family", "Helvetica") ]
+
+
+inputStyle =
     style
-    [
-      ( "font-size", "2em" ),
-      ( "color", "#333" ),
-      ("padding-bottom", "30px")
-    ]
+        [ ( "padding-top", "16px" )
+        , ( "padding-bottom", "6px" )
+        , ( "width", "188px" )
+        , ( "outline", "none" )
+        , ( "color", "#000" )
+        , ( "font-size", "16px" )
+        , ( "font-weight", "400" )
+        , ( "border", "none" )
+        , ( "border-bottom", "2px solid #99ddff" )
+        ]
 
-buttonStyling =
-    style
-      [
-        ( "padding", "10px" ),
-        ( "background-color", "#99ddff" ),
-        ( "border-radius", "2px" ),
-        ( "border", "1px solid #99ddff" ),
-        ( "color", "white" ),
-        ( "font-size", "1.5em" ),
-        ( "cursor", "pointer" ),
-        ( "margin", "5px")
-      ]
 
-imageStyling =
+titleStyle =
+    style [ ( "font-size", "2em" ), ( "color", "#333" ) ]
+
+
+buttonStyle =
     style
-      [
-        ( "border-radius", "2px" ),
-        ( "box-shadow", "0 2px 4px rgba(0,0,0,0.12), 0 2px 3px rgba(0,0,0,0.24)")
-      ]
+        [ ( "padding", "10px" )
+        , ( "background-color", "#99ddff" )
+        , ( "border-radius", "2px" )
+        , ( "border", "1px solid #99ddff" )
+        , ( "color", "white" )
+        , ( "font-size", "1.5em" )
+        , ( "cursor", "pointer" )
+        ]
+
+
+imgStyle =
+    style
+        [ ( "border-radius", "2px" )
+        , ( "box-shadow"
+          , "0 2px 4px rgba(0,0,0,0.12), 0 2px 3px rgba(0,0,0,0.24)"
+          )
+        ]
